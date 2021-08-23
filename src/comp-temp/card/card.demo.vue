3ae45077fab51fedcb17a335f686678f43a28872
陈桥黎 <cql.ql@qq.com>
2021-08-23 20:28:07 +0800

    feat[temp]: card

---
+++ src/comp-temp/card/card.demo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="card-right-fixed">
+    <div class="card-right-fixed__left">
+      <div class="card-right-fixed__tit">title</div>
+      <div class="card-right-fixed__sub">200M</div>
+    </div>
+    <div class="card-right-fixed__right">
+      <img
+        src=""
+        alt=""
+      >
+    </div>
+  </div>
+</template>
