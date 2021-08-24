ce584280155ae02ce1c0a87da32c338a4f4c0891
陈桥黎 <cql.ql@qq.com>
2021-08-24 14:24:15 +0800

    fix[temp]: card

--- src/comp-temp/card/card.demo.vue
+++ src/comp-temp/card/card.demo.vue
@@ -12,3 +12,26 @@
     </div>
   </div>
 </template>
+<style scoped>
+.card-right-fixed {
+  display: flex;
+}
+
+.card-right-fixed__left {
+  flex: 1;
+}
+.card-right-fixed__tit {}
+
+.card-right-fixed__sub {
+  color: #aaa;
+  font-size: 12px;
+}
+
+.card-right-fixed__right {
+  font-size: 28px;
+
+  /* padding: 0 5px; */
+  padding-left: 5px;
+  color: #888;
+}
+</style>
