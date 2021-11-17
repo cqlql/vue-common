f268fad9ddd7ac9fb04a74da2766242223128170
陈桥黎 <cql.ql@qq.com>
2021-11-17 10:40:07 +0800

    feat: 数字输入

--- src/components/InputPlus/demo/index.vue
+++ src/components/InputPlus/demo/index.vue
@@ -1,14 +1,29 @@
 <template>
   <div>
-    <InputPlus keep="\d*\.?\d*" :replace="replace" />
+    <InputPlus :test="test" v-model="val" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
 import InputPlus from '../index.vue'
 
-function replace(v: string) {
-  return v
+let val = ref('')
+
+let reg = /^\d+\.?\d*$/
+function test(v: string) {
+  let pass
+
+  pass = reg.test(v)
+
+  if (pass) {
+    pass = Number(v) >= 100
+    if (!pass) {
+      val.value = '100'
+    }
+  }
+
+  return pass
 }
 </script>
 <style lang="scss"></style>
--- src/components/InputPlus/index.vue
+++ src/components/InputPlus/index.vue
@@ -1,17 +1,40 @@
 <template>
   <div>
-    <input type="text" @input="onInput" />
+    <input type="text" @input="onInput" :value="modelValue" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
+
 const props = defineProps<{
-  keep: string
+  modelValue: string
+  test(v: string): boolean
 }>()
-const reg = new RegExp(props.keep)
-function onInput(e: Event) {
-  let target = (e as InputEvent).target as HTMLInputElement
-  target.value = target.value.match(reg)?.[0] || ''
-}
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', v: string): void
+}>()
+
+const { /*  val,  */ onInput } = (() => {
+  // const val = ref(props.modelValue)
+
+  function onInput(e: Event) {
+    let target = (e as InputEvent).target as HTMLInputElement
+    let nowVal = target.value
+    console.log(nowVal)
+    if (props.test(nowVal)) {
+      // val.value = nowVal
+      emit('update:modelValue', nowVal)
+    } else {
+      // target.value = val.value
+      target.value = props.modelValue
+    }
+  }
+  return {
+    // val,
+    onInput,
+  }
+})()
 </script>
 <style lang="scss"></style>
