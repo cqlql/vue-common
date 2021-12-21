028e474d97ef564318f75c80bd59e7b066c4b42a
陈桥黎 <cql.ql@qq.com>
2021-12-21 19:44:10 +0800

    Update CountDown.vue

--- src/components/CountDown/CountDown.vue
+++ src/components/CountDown/CountDown.vue
@@ -1,5 +1,6 @@
 <template>
   <div :class="{ emergency: isEmergency }"> {{ time }} </div>
+  <div :class="['CountDown_box', boxClass]">1</div>
 </template>
 
 <script lang="ts" setup>
@@ -7,6 +8,7 @@ import { ref } from 'vue'
 
 const isEmergency = ref(false)
 const time = ref('7')
+const boxClass = ref('')
 
 let timeValue = Number(time.value)
 
@@ -24,11 +26,30 @@ const timeId = setInterval(() => {
   if (timeValue <= 0) {
     clearInterval(timeId)
     console.log('停止了')
+    boxClass.value = 'zoom'
   }
-}, 400)
+}, 100)
 </script>
 <style lang="scss">
 .emergency {
   color: red;
+}
+
+.CountDown_box {
+  height: 50px;
+  width: 50px;
+  background-color: #aaa;
+}
+
+.zoom {
+  animation: zoom 1s ease-out;
+}
+@keyframes zoom {
+  0% {
+    transform: scale(1);
+  }
+  to {
+    transform: scale(1.5);
+  }
 }
 </style>
