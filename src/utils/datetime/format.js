08678b9ea833ed445aa6eea6712cd7e2933e0a70
陈桥黎 <cql.ql@qq.com>
2022-01-30 16:43:17 +0800

    feat[fillZero]: 补零

--- src/utils/datetime/format.js
+++ src/utils/datetime/format.js
@@ -1,52 +1,62 @@
 export const formatReg = /YYYY|MM|DD|HH|hh|mm|ss|a|dddd/g
 
-function fillDouble (num) {
+export function fillDouble(num) {
   if (num < 10) {
     return '0' + num
   }
   return num
 }
 
+// 补指定数量的 0
+export function fillZero(numberValue, length = 2) {
+  numberValue = String(numberValue)
+  let zeroNumber = length - numberValue.length
+  if (zeroNumber) {
+    return new Array(zeroNumber + 1).join('0') + numberValue
+  }
+  return numberValue
+}
+
 export default class Format {
-  constructor (date) {
+  constructor(date) {
     this.date = date
   }
 
-  YYYY () {
+  YYYY() {
     return this.date.getFullYear()
   }
 
-  MM () {
+  MM() {
     return fillDouble(this.date.getMonth() + 1)
   }
 
-  DD () {
+  DD() {
     return fillDouble(this.date.getDate())
   }
 
-  HH () {
+  HH() {
     return fillDouble(this.date.getHours())
   }
 
-  hh () {
+  hh() {
     const hour = this.date.getHours() % 12
     return fillDouble(hour || 12)
   }
 
-  a () {
+  a() {
     const hour = this.date.getHours()
     return hour < 12 ? '上午' : '下午'
   }
 
-  mm () {
+  mm() {
     return fillDouble(this.date.getMinutes())
   }
 
-  ss () {
+  ss() {
     return fillDouble(this.date.getSeconds())
   }
 
-  dddd () {
-    return '星期' + ('天一二三四五六'[this.date.getDay()])
+  dddd() {
+    return '星期' + '天一二三四五六'[this.date.getDay()]
   }
 }
