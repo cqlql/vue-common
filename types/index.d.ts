0db7705fd33d5c030d564a96b9956d3d16ea53a2
陈桥黎 <cql.ql@qq.com>
2023-04-12 22:51:32 +0800

    chore: types

--- types/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare type ElRef<T extends HTMLElement = HTMLDivElement> = Nullable<T>
-declare module 'vue-i18n/index' {
-  import { createI18n } from 'vue-i18n'
-  export { createI18n }
-}
-
-interface Location {
-  reload(forcedReload: boolean): void
-}
