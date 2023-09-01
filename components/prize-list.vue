d900e213c37ddd29d5a81e8dbd15442d31bac0ea
陈桥黎 <cql.ql@qq.com>
2023-09-01 13:23:44 +0800

    换成动态尺寸

--- components/prize-list.vue
+++ components/prize-list.vue
@@ -1,32 +1,36 @@
-<template>
-  <view class="prize-list">
-    <view v-for="i of 5" class="prize-item">
-      <div class="box">
-        <view class="tag"></view>
-        <view class="img-wrap">
-          <image
-            class="img"
-            src="https://dxxshoptest.oss-cn-shenzhen.aliyuncs.com/backManager/bd08d463-6610-4066-a9cc-d763e522094a.png"
-          ></image>
-        </view>
-        <view class="name">
-          小米充电器头30W快充10s红米k40k30i闪充33w瓦note9K20cc9pro手机18w头6正品7数据线科瑞信原装27w8typec
-        </view>
-      </div>
-      <div class="btn">
-        <view class="label">一键分解</view>
-        <view class="val">
-          <image
-            class="ico"
-            src="/static/image/result/icon-qingtuan.png"
-          ></image>
-          1811.9</view
-        >
-      </div>
-    </view>
-  </view>
-</template>
 
+
+<template>
+  <div class="CardList small" >
+    <div v-for="i of 4" class="CardItem">
+      <div class="box">
+        <div class="tag">
+          <image
+            class="tag-img"
+            src="/pagesA/static/tag-1.png"
+          ></image>
+          <div class="text">
+            普通
+          </div>
+        </div>
+        <div class="img-wrap">
+          <image mode="aspectFit" class="img" src="https://dxxshoptest.oss-cn-shenzhen.aliyuncs.com/backManager/bd08d463-6610-4066-a9cc-d763e522094a.png"></image>
+        </div>
+        <div class="name">
+          小米充电器头30W快充10s红米k40k30i闪充33w瓦note9K20cc9pro手机18w头6正品7数据线科瑞信原装27w8typec
+        </div>
+      </div>
+      <div class="bott">
+        <div class="label">一键分解</div>
+        <div class="val">
+          <image class="ico" src="/static/image/result/icon-qingtuan.png"></image>
+          1811.9
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
 <script>
 export default {
   components: {},
@@ -40,71 +44,141 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.prize-list {
-  display: flex;
-  margin: 60rpx 0;
-  flex-wrap: wrap;
-  gap: 24rpx;
-  justify-content: center;
-  color: rgba(0, 0, 0, 0.9);
-}
-.prize-item {
-  .box {
-    position: relative;
-    width: 230rpx;
-    height: 270rpx;
-    background-color: #fff;
-    border: 2rpx solid #705ed1;
-    box-shadow: inset 0 0 20px 0px #83f0f3, 0 0 4rpx 0 #00000045;
-    border-radius: 10rpx;
-  }
-  .name {
-    // position: absolute;
-    // bottom: 40rpx;
-    // width: 80%;
-    // left: 10%;
-    text-align: center;
-    font-size: 26rpx;
+$CardItemWidth:115px;
+$CardItemHeight:135px;
+$CardIListGap:10px;
 
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    padding: 20rpx 16rpx;
-    font-weight: 700;
-  }
-  .img-wrap {
+$BottomMarginTop:8px;
+$CardItemBorderRadius:5px;
+$ImgWidth:80px;
+$ImgHeight:86px;
+$ImgMarginTop:10px;
+$NameTop:10px;
+$NameLeft:10px;
+
+$TagWidth:48px;
+$TagHeight:48px;
+$TagLeft:-2px;
+$TagTop:-2px;
+$borderWidth:1px;
+.CardList {
+  --CardItemWidth:#{$CardItemWidth};
+  --CardItemHeight:#{$CardItemHeight};
+  --CardIListGap:#{$CardIListGap};
+  
+  --BottomMarginTop:#{$BottomMarginTop};
+  --CardItemBorderRadius:#{$CardItemBorderRadius};
+  --ImgWidth:#{$ImgWidth};
+  --ImgHeight:#{$ImgHeight};
+  --ImgMarginTop:#{$ImgMarginTop};
+  --NameTop:#{$NameTop};
+  --NameLeft:#{$NameLeft};
+
+  --TagWidth:#{$TagWidth};
+  --TagHeight:#{$TagHeight};
+  --TagLeft:#{$TagLeft};
+  --TagTop:#{$TagTop};
+
+  --borderWidth:#{$borderWidth};
+
+  display: flex;
+  gap: var(--CardIListGap);
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+$smallScale:0.7;
+.CardList.small {
+  --CardItemWidth:#{$CardItemWidth*$smallScale};
+  --CardItemHeight:#{$CardItemHeight*$smallScale};
+  --CardIListGap:#{$CardIListGap*$smallScale};
+  
+  --BottomMarginTop:#{$BottomMarginTop*$smallScale};
+  --CardItemBorderRadius:#{$CardItemBorderRadius*$smallScale};
+  --ImgWidth:#{$ImgWidth*$smallScale};
+  --ImgHeight:#{$ImgHeight*$smallScale};
+  --ImgMarginTop:#{$ImgMarginTop*$smallScale};
+  --NameTop:#{$NameTop*$smallScale};
+  --NameLeft:#{$NameLeft*$smallScale};
+
+  --TagWidth:#{$TagWidth*$smallScale};
+  --TagHeight:#{$TagHeight*$smallScale};
+  --TagLeft:#{$TagLeft*$smallScale};
+  --TagTop:#{$TagTop*$smallScale};
+
+  --borderWidth:#{$borderWidth*$smallScale};
+}
+
+.CardItem {
+  text-align: center;
+
+  .box {
+    width: var(--CardItemWidth);
+    height: var(--CardItemHeight);
+    border-radius: var(--CardItemBorderRadius);
+    background-color: #fff;
     position: relative;
-    // width: 70%;
-    padding-bottom: 70%;
-    // left: 15%;
-    margin: 40rpx 40rpx 0;
+    border: var(--borderWidth) solid #705ed1;
+    box-shadow: inset 0 0 20px 0px #83f0f3, 0 0 2px 0 #00000045;
+  }
+
+  .tag {
+    position: absolute;
+    width: var(--TagWidth);
+    height: var(--TagHeight);
+    left: var(--TagLeft);
+    top: var(--TagTop);
+
+    .text {
+      position: absolute;
+      top: 28%;
+    left: 28%;
+      transform: translateX(-50%) translateY(-50%) rotate(-45deg);
+      color: #fff;
+      font-size: 12px;
+      white-space: nowrap;
+    }
+
+    .tag-img {
+      width: 100%;
+      height: 100%;
+    }
+
+    &.normal{}
+    &.rare{}
+    &.supreme{}
+    &.legend{}
+  }
+
+  .img-wrap {
+    width: var(--ImgWidth);
+    height: var(--ImgHeight);
+    margin: var(--ImgMarginTop) auto 0;
     background-color: #fff;
   }
 
   .img {
-    position: absolute;
     width: 100%;
     height: 100%;
   }
 
-  .btn {
-    margin-top: 16rpx;
-    border: 1px solid #000;
-    background-color: #fff;
-    font-weight: 700;
-    text-align: center;
-    border-radius: 8rpx;
+  .name {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    margin: var(--NameTop) var(--NameLeft) 0;    font-weight: 700;
+  }
 
-    .label {
+  .bott {
+    border-radius: var(--CardItemBorderRadius);
+    margin-top: var(--BottomMarginTop);
+    border: var(--borderWidth) solid #000;
+    background-color: #fff;
+    .label{
       font-weight: 700;
     }
-    .val {
+    .val{
       font-weight: 700;
-    }
-    .ico {
-      width: 20rpx;
-      height: 20rpx;
-      display: inline-block;
     }
   }
 }
