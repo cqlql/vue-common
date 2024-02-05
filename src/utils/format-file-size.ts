3fada7c1d7f9d01d3b8988ab3c777f2bfb179e11
陈桥黎 <cql.ql@qq.com>
2024-02-05 11:09:21 +0800

    fix: 文件大小格式化可为空

--- src/utils/format-file-size.ts
+++ src/utils/format-file-size.ts
@@ -1,12 +1,16 @@
-export default function formatFileSize(size: number) {
-  const units = ['B', 'KB', 'MB', 'GB', 'TB']
-  let unitIndex = 0
+export default function formatFileSize(size?: number | string) {
+  if (size === undefined || size === null) {
+    return '0B';
+  }
+  size = Number(size);
+  const units = ['B', 'KB', 'MB', 'GB', 'TB'];
+  let unitIndex = 0;
   while (size >= 1024 && unitIndex < units.length - 1) {
-    size /= 1024
-    unitIndex++
+    size /= 1024;
+    unitIndex++;
   }
   // 四舍五入
-  return Number(size.toFixed(1)) + units[unitIndex]
+  return Number(size.toFixed(1)) + units[unitIndex];
   // 不四舍五入
   // return Math.floor(size * 10) / 10 + units[unitIndex]
 }
