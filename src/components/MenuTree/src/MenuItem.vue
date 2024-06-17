a83faf6f0730c38e39ef0d7ec1b63a71796d0ff5
陈桥黎 <cql.ql@qq.com>
2024-06-17 17:14:59 +0800

    refactor: MenuTree 展开动画改用手写，不使用 auto-animate

--- src/components/MenuTree/src/MenuItem.vue
+++ src/components/MenuTree/src/MenuItem.vue
@@ -1,6 +1,4 @@
 <script lang="ts" setup>
-// import { useAutoAnimate } from '@formkit/auto-animate/vue'
-import { vAutoAnimate } from '@formkit/auto-animate'
 import MenuList from './MenuList.vue'
 import type { MenuDataItem } from './type'
 const props = defineProps<{
@@ -9,16 +7,53 @@ const props = defineProps<{
 
 const expanded = ref(false)
 const hasChildren = computed(() => !!props.item.children?.length)
+const vMenuListRef = ref<InstanceType<typeof MenuList>>()
+const eMenuList = computed(() => {
+  return vMenuListRef.value?.$el
+})
+
+async function onClickItem() {
+  if (!hasChildren.value) return
+  expanded.value = !expanded.value
+
+  const eMenuListStyle = eMenuList.value!.style
+  const eMenuListClassList = eMenuList.value!.classList
+
+  if (expanded.value) {
+    eMenuListStyle.height = 'auto'
+    eMenuListStyle.display = 'block'
+    const h = eMenuList.value.clientHeight
+    eMenuListStyle.height = '0'
+    eMenuListClassList.add('animated')
+    await new Promise((resolve) => setTimeout(resolve, 0))
+    eMenuListStyle.height = `${h}px`
+  } else {
+    const h = eMenuList.value.clientHeight
+    eMenuListStyle.height = `${h}px`
+    eMenuListClassList.add('animated')
+    await new Promise((resolve) => setTimeout(resolve, 0))
+    eMenuListStyle.height = '0'
+  }
+
+  eMenuList.value.addEventListener('transitionend', () => {
+    eMenuListClassList.remove('animated')
+    if (expanded.value) {
+      eMenuListStyle.height = 'auto'
+    } else {
+      eMenuListStyle.display = 'none'
+    }
+  })
+}
 </script>
 <template>
-  <div class="MenuItem" v-auto-animate :class="{ expanded }">
-    <div class="title-content" @click="expanded = !expanded">
+  <div class="MenuItem" :class="{ expanded }">
+    <div class="title-content" @click="onClickItem">
       <div class="cont">
         <span>{{ item.name }}</span>
-        <span v-if="hasChildren" class="arrows"><IconEpArrowDownBold /></span>
+        <span v-if="hasChildren" class="arrows"><IconEpArrowDownBold class="icon" /></span>
       </div>
     </div>
-    <MenuList v-if="hasChildren && expanded" class="MenuList" :list="item.children!" />
+    <MenuList v-if="hasChildren" ref="vMenuListRef" class="MenuList" :list="item.children!" />
   </div>
 </template>
 
@@ -58,11 +93,24 @@ const hasChildren = computed(() => !!props.item.children?.length)
       color: #8f959e;
       transition: transform 0.4s cubic-bezier(0.34, 0.69, 0.1, 1);
     }
+
+    .icon {
+      display: block;
+    }
   }
 
   & > .MenuList {
-    // display: none;
+    display: none;
     padding-left: 20px;
+
+    // overflow: hidden;
+
+    &.animated {
+      overflow: hidden;
+      transition:
+        height 0.2s cubic-bezier(0.645, 0.045, 0.355, 1),
+        opacity 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
+    }
   }
 
   &.expanded > .title-content {
